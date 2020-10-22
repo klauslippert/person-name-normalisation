5fb52da5da349e0befe91cbb58e6729a34437772
klaus.lippert <lippert@zbmed.de>
2020-10-22 21:50:50 +0200

    release 0.2

--- personnamenorm/__init__.py
+++ personnamenorm/__init__.py
@@ -5,7 +5,7 @@ __author__ = "Klaus Lippert"
 __copyright__ = "2020 by Klaus Lippert"
 __license__ = "MIT"
 __email__ = "klaus.lippert@mailbox.org"
-__version__ = "0.1f"
+__version__ = "0.2"
 
 from .namenorm import *
 
