a11ccb101d5f13f6bd71bd911f6ddf3ad019d7de
klaus.lippert <lippert@zbmed.de>
2020-11-24 10:00:50 +0100

    removed wipo-alpha. replaced umlaute

--- NaiveBayes_on_names/main.py
+++ NaiveBayes_on_names/main.py
@@ -10,29 +10,39 @@ def save_to_disc(df,name):
     print(f'{timenow()}  -  done')
     
 
-df_wipo_a = WIPO_a('data/raw/names_raw_WIPOalpha.csv').df
-_=save_to_disc(df_wipo_a,"data/proc/df_wipo_a.p")
+#df_wipo_a = WIPO_a('data/raw/names_raw_WIPOalpha.csv').df
+#_=save_to_disc(df_wipo_a,"data/proc/df_wipo_a.p")
 
-df_dime   = DIME('data/raw/names_raw_DIME.csv').df
-_=save_to_disc(df_dime,"data/proc/df_dime.p")
+#df_dime   = DIME('data/raw/names_raw_DIME.csv').df
+#_=save_to_disc(df_dime,"data/proc/df_dime.p")
 
-df_orcid  = ORCID('data/raw/names_first_raw_ORCID.csv','data/raw/names_last_raw_ORCID.csv').df
-_=save_to_disc(df_orcid,"data/proc/df_orcid.p")
+#df_orcid  = ORCID('data/raw/names_first_raw_ORCID.csv','data/raw/names_last_raw_ORCID.csv').df
+#_=save_to_disc(df_orcid,"data/proc/df_orcid.p")
 
-df_pmc = PMC('data/raw/names_raw_PMC.csv').df
-_=save_to_disc(df_pmc,"data/proc/df_pmc.p")
+#df_pmc = PMC('data/raw/names_raw_PMC.csv').df
+#_=save_to_disc(df_pmc,"data/proc/df_pmc.p")
 
 
 
-#df_dime   = pickle.load(open("data/proc/df_dime.p","rb"))
+df_dime   = pickle.load(open("data/proc/df_dime.p","rb"))
 #df_wipo_a = pickle.load(open("data/proc/df_wipo_a.p","rb"))
-#df_orcid  = pickle.load(open("data/proc/df_orcid.p","rb"))
-#df_pmc    = pickle.load(open("data/proc/df_pmc.p","rb"))
+df_orcid  = pickle.load(open("data/proc/df_orcid.p","rb"))
+df_pmc    = pickle.load(open("data/proc/df_pmc.p","rb"))
 
 ## concat
 print(f'{timenow()}  -  concat all datasets')
-dfall=pd.concat([df_dime,df_orcid,df_wipo_a,df_pmc])
+dfall=pd.concat([df_dime,df_orcid,df_pmc])
 print(f'{timenow()}  -  done  ->  {len(dfall)} usable names')
+
+## remove umlaute
+def replace_umlaute(txt):
+    umlaute = {'ä':'ae','ö':'oe','ü':'ue',
+               'Ä':'Ae','Ö':'Oe','Ü':'Ue',
+               'ß':'ss'}
+    for umlaut in umlaute.keys():
+        txt = txt.replace(umlaut,umlaute[umlaut])
+    return txt
+dfall['total'] = [replace_umlaute(x) for x in dfall['total']]
 
 #legacy
 #_ = print_infos(df_dime,df_orcid,df_wipo_a,dfall)
@@ -53,6 +63,11 @@ dfall['firstname']=[[x for x in y if isinitial(x) is False] \
 list_last  = [item.lower() for sublist in dfall['raw_lastname'].to_list() for item in sublist]
 list_first = [item.lower() for sublist in dfall['firstname'].to_list() for item in sublist]
 
+## remove umlaute
+list_last  = [replace_umlaute(x) for x in list_last]
+list_first = [replace_umlaute(x) for x in list_first]
+
+## count
 dict_lastnames  = Counter(list_last)
 dict_firstnames = Counter(list_first)
 
---
+++ NaiveBayes_on_names/table_usable_names.md
@@ -0,0 +1,17 @@
+
+| name     | raw         | usable     | unique   | in use | 
+|---       |---          |---         |---       |---     |
+|DIME      |  25.571.894 | 15.685.184 |          |  yes   |
+|ORCID     |   9.175.106 |  9.175.106 |          |  yes   |
+|PMC       | 270.819.112 | 13.571.217 |          |  yes   |
+|WIPO-a    |     160.149 |    160.149 |          |   no   |
+| DBLP     |  18.937.273 |            |          |   no   |
+| wikidata |   8.414.178 |            |          |   no   |
+|          |             |            |          |        |
+| TOTAL    |             | 38.431.507 |24.105.518|        |
+
+
+
+| unique firstnames | unique lastnames | total unique |
+|---                |---               |---           |
+| 1.679.114         | 2.199.874        | 3.180.595    |
